098e3a20ddb6a921df34b4d890b310b0645af4f1
Lizp <lzpthu@163.com>
2017-03-01 22:47:22 +0800

    The first code

---
+++ TheFirstCode/ActivityTest/app/src/main/java/com/example/lizp/activitytest/ThirdACtivity.java
@@ -0,0 +1,13 @@
+package com.example.lizp.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdACtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
