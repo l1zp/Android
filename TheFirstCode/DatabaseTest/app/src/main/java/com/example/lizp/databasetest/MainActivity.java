f200754903eac472661363158d1005dc107683a1
Lizp <lzpthu@163.com>
2017-03-25 22:01:55 +0800

    database

---
+++ TheFirstCode/DatabaseTest/app/src/main/java/com/example/lizp/databasetest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.lizp.databasetest;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private MyDatabaseHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 10);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button addData = (Button) findViewById(R.id.add_data);
+        Button updateData = (Button) findViewById(R.id.update_data);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbHelper.getWritableDatabase();
+            }
+        });
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "The Lost Symbol");
+                values.put("anthor", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+            }
+        });
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("Book", values, "name = ?", new String[] {
+                        "The Da Vinci Code"});
+            }
+        });
+    }
+}
--- TheFirstCode/FilePersistenceTest/app/src/main/java/com/example/lizp/filepersistencetest/MainActivity.java
+++ TheFirstCode/FilePersistenceTest/app/src/main/java/com/example/lizp/filepersistencetest/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.lizp.filepersistencetest;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edit = (EditText) findViewById(R.id.edit);
+        String inputText = load();
+        if (!TextUtils.isEmpty(inputText)) {
+            edit.setText(inputText);
+            edit.setSelection(inputText.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -46,5 +58,30 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public String load() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
