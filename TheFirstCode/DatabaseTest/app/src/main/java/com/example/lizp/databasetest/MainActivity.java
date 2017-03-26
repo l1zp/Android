f197f216ebee08c5c1bb15e55c1ea2309b82efc3
Lizp <lzpthu@163.com>
2017-03-26 19:03:22 +0800

    database litepal

--- TheFirstCode/DatabaseTest/app/src/main/java/com/example/lizp/databasetest/MainActivity.java
+++ TheFirstCode/DatabaseTest/app/src/main/java/com/example/lizp/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.lizp.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Button createDatabase = (Button) findViewById(R.id.create_database);
         Button addData = (Button) findViewById(R.id.add_data);
         Button updateData = (Button) findViewById(R.id.update_data);
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        Button queryData = (Button) findViewById(R.id.query_data);
         createDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 db.insert("Book", null, values);
                 values.clear();
                 values.put("name", "The Lost Symbol");
-                values.put("anthor", "Dan Brown");
+                values.put("author", "Dan Brown");
                 values.put("pages", 510);
                 values.put("price", 19.95);
                 db.insert("Book", null, values);
@@ -49,6 +53,36 @@ public class MainActivity extends AppCompatActivity {
                 values.put("price", 10.99);
                 db.update("Book", values, "name = ?", new String[] {
                         "The Da Vinci Code"});
+            }
+        });
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[] {"500"});
+            }
+        });
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex
+                                ("name"));
+                        String author = cursor.getString(cursor.getColumnIndex
+                                ("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex
+                                ("price"));
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+                    } while (cursor.moveToNext());
+                    cursor.close();
+                }
             }
         });
     }
