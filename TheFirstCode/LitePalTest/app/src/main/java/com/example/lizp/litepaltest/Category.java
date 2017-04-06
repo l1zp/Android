4e28ea4e877d176a8b94b9c5758c395bd600b4fe
Lizp <lzpthu@163.com>
2017-04-06 20:27:44 +0800

    finish charpter6

---
+++ TheFirstCode/LitePalTest/app/src/main/java/com/example/lizp/litepaltest/Category.java
@@ -0,0 +1,23 @@
+package com.example.lizp.litepaltest;
+
+/**
+ * Created by Lizp on 2017/4/6.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
--- TheFirstCode/LitePalTest/app/src/main/java/com/example/lizp/litepaltest/MainActivity.java
+++ TheFirstCode/LitePalTest/app/src/main/java/com/example/lizp/litepaltest/MainActivity.java
@@ -2,6 +2,15 @@ package com.example.lizp.litepaltest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.tablemanager.Connector;
+import org.litepal.util.BaseUtility;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Connector.getDatabase();
+            }
+        });
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setName("The Da Vinci Code");
+                book.setAuthor("Dan Brown");
+                book.setPages(454);
+                book.setPrice(16.96);
+                book.setPress("Unknow");
+                book.save();
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Book book = new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?", "The Lost Symbol", "Dan Brown");
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DataSupport.deleteAll(Book.class, "price < ?", "15");
+            }
+        });
+
+        Button queryData = (Button) findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Book> books = DataSupport.findAll(Book.class);
+                for (Book book : books) {
+                    Log.d("MainActivity", "book name is " + book.getName());
+                    Log.d("MainActivity", "book author is " + book.getAuthor());
+                    Log.d("MainActivity", "book pages is " + book.getPages());
+                    Log.d("MainActivity", "book price is " + book.getPrice());
+                    Log.d("MainActivity", "book press is " + book.getPress());
+                }
+            }
+        });
     }
 }
